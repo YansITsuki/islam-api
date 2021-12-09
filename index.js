3736b21d694f4f0a341355a7f8beb680251f8b5d
YansItsuki <gantengcuy846@gmail.com>
2021-12-09 11:09:40 +0700

    anjay

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const cors = require('cors');
 const secure = require('ssl-express-www');
 const mainrouter = require('./routes/main');
 const apirouter = require('./routes/api');
+const quranrouter = require('./quran/router.js');
 
 const PORT = process.env.PORT || 8080 || 5000 || 3000;
 const app = express();
-
 app.enable('trust proxy');
 app.set('json spaces', 2);
 app.use(cors());
 app.use(secure);
 app.use(express.static('public'));
-
+app.use((_, res, next) => {
+    res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=86400, stale-while-revalidate')
+    next()
+})
+app.use(express.json())
 app.use('/', mainrouter);
 app.use('/api', apirouter);
+app.use('/api/quran', quranrouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
